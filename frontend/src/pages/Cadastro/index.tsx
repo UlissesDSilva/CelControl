c5c933483d04e8c774c9a66806daacfbb5ac111a
João Victor <jvdias79797@gmail.com>
2021-08-17 21:57:26 -0300

    fix: returns idcelula on GET /celula

--- frontend/src/pages/Cadastro/index.tsx
+++ frontend/src/pages/Cadastro/index.tsx
@@ -1,69 +1,32 @@
 import React, { useState } from 'react'
-import { useParams } from 'react-router'; 
+import { useParams } from 'react-router';
+import { useHistory } from 'react-router-dom' 
 import { api } from '../../services/api';
-import { Form, Input, Button, Checkbox, Divider } from 'antd';
+import { Form, Input, Button, Select, Radio } from 'antd';
+import { useEffect } from 'react';
 
-const horas = [
-  'M-AB',
-  'M-CD',
-  'T-AB',
-  'T-CD',
-  'N-AB',
-  'N-CD',
-]
-
-const horarios = [
-  { 
-    weekDay: 'SEG',
-    horas
-  },
-  {
-    weekDay: 'TER',
-    horas
-  },
-  {
-    weekDay: 'QUA',
-    horas
-  },
-  {
-    weekDay: 'QUI',
-    horas
-  },
-  {
-    weekDay: 'SEX',
-    horas
-  },
-];
-
-type Schedules = {
-  weekDay: string,
-  hours: []
-}
-const weekDay = horarios;
+const { Option } = Select;
 
 
 export function CadastroAluno() {
 
+  const [celulasHours, setCelulasHours] = useState('')
+  const history = useHistory()
   const params = useParams() as {id: string}
   const idCel = params.id
-
-  const [semana, setSemana] =  useState<Schedules[]>([])
-  // const [ disabled, setDisabled ] = useState(true)
   
-  function onChangeWeek(event: any) {
-    setSemana({
-      ...event,
-      weekDay: event
-    })    
-  }
+  useEffect(() => {
+    api.get(`/celula/${idCel}`)
+      .then(res => setCelulasHours(res.data.dias_celulas))
+      .catch(err => console.log(err))
+  }, [idCel])
 
-  function onChangeHours(event: any) {
-    setSemana({
-      ...event,
-      hours: event
-    })
+  const [ disabled, setDisabled ] = useState(true)
+
+  function handleConfirm() {
+    alert("Você confirma que não está matrículado em disciplinas no horário da célula!?")
+    setDisabled(!disabled)
   }
-  console.log(semana);
 
   async function onFinish (values: any) {
     const data = {
@@ -76,11 +39,11 @@ export function CadastroAluno() {
       const student = response.data.id
       await api.post('/celulaStudent', {student, celula: idCel})
       alert("Aluno CAdastrado!")
+      history.push('/')
     } catch (error) {
       console.log(error);
       
     }
-    console.log('Received values of form: ', values);
   };
 
   return (
@@ -127,23 +90,12 @@ export function CadastroAluno() {
           >
             <Input.Password placeholder="Senha" />
           </Form.Item>
-          <div style={{display: 'flex',  border: '1px solid #ccc', borderRadius: '20px', width: 'max-content', padding: '15px'}}>
-            {horarios.map((horario, indice: number) => (
-              <div style={{display: 'flex'}} key={horario.weekDay}>
-                <div>
-                  <Checkbox onChange={(event) => onChangeWeek(event.target.value)} value={horario.weekDay}>
-                    {horario.weekDay}
-                  </Checkbox>
-                  <Checkbox.Group options={horas} style={{display: 'flex', flexDirection: 'column'}} onChange={onChangeWeek} />
-                </div>
-                {horarios.length - 1 > indice && (<Divider type='vertical' style={{height: '100%'}}/>)}
-              </div>
-            ))}
-          </div>
-          <br />
-                  
+          <div style={{display: 'flex', flexDirection: 'column'}}>
+            <strong> Horário da célula {celulasHours}</strong>
+            <Radio onChange={() => handleConfirm()}> Confirmação de horários </Radio>
+          </div>        
           <Form.Item label=" " colon={false} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-            <Button type="primary" htmlType="submit" style={{background: '#00AFEF', borderRadius: '50px'}}>
+            <Button type="primary" htmlType="submit" style={{background: '#00AFEF', borderRadius: '50px'}} disabled={disabled}>
               Cadastrar
             </Button>
           </Form.Item> 
@@ -154,19 +106,85 @@ export function CadastroAluno() {
   );
 };
 
+
+const weekDaySelect = [
+  {
+    day: 'Segunda-Feira',
+    sig: 'SEG'
+  },
+  {
+    day: 'Terça-Feira',
+    sig: 'TER'
+  },
+  {
+    day: 'Quarta-Feira',
+    sig: 'QUA'
+  },
+  {
+    day: 'Quinta-Feira',
+    sig: 'Qui'
+  },
+  {
+    day: 'Sexta-Feira',
+    sig: 'SEX'
+  },
+]
+
+const hoursSelect = [
+  { hour: '08:00', sig: 'MAB' },
+  { hour: '10:00', sig: 'MCD' },
+  { hour: '13:30', sig: 'TAB' },
+  { hour: '15:30', sig: 'TCD' },
+  { hour: '18:00', sig: 'NAB' },
+  { hour: '20:00', sig: 'NCD' }
+]
+
 // Cadastro de célula
 export function CadastroCelula() {
 
-  function onChangeSemana(event: any) {
-    
-    console.log(event);
+  function renderWeekDaySelect() {
+    return weekDaySelect.map(day => {
+      return(
+        <Option key={day.sig} value={day.sig}>{day.day}</Option>
+      )
+    })
   }
-  const onFinish = (values: any) => {
-    console.log('Received values of form: ', values);
+
+  function renderHoursSelect() {
+    return hoursSelect.map((hour, indice) => {
+      return(
+        <Option key={indice} value={hour.sig}>{hour.hour}</Option>
+      )
+    })
+  }
+  
+  const [dayCel, setDayCel] = useState('')
+
+  function onChangeSemana(event: any) {
+    setDayCel(event)
+  }
+
+  function onChangeHours(event: any){
+    const hour = event
+    setDayCel(`${dayCel}-${hour}`)
+  }
+  
+  async function onSubmit(values: any) {
+    const data = {
+      ...values,
+      hours: 2,
+      diasCelulas: dayCel
+    }
+    try {
+      await api.post('/celula', data) 
+      alert("Célula cadastrada")
+    } catch (error) {
+      console.log(error)
+    }
   };
 
   return (
-    <Form name="complex-form" onFinish={onFinish} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} style={{margin: '40px auto', maxWidth: '80vw'}}>
+    <Form name="complex-form" onFinish={onSubmit} labelCol={{ span: 8 }} wrapperCol={{ span: 16 }} style={{margin: '40px auto', maxWidth: '80vw'}}>
       <Form.Item label="Info. da Célula" style={{ marginBottom: 0 }}>
         <Form.Item
           name="name"
@@ -176,7 +194,7 @@ export function CadastroCelula() {
           <Input placeholder="Nome da Célula" />
         </Form.Item>
         <Form.Item
-          name="Facilitador"
+          name="facilitator"
           rules={[{ required: true }]}
           style={{ display: 'inline-block', width: '25%', margin: '0 8px' }}
         >
@@ -192,38 +210,33 @@ export function CadastroCelula() {
       </Form.Item>
       <Form.Item label="Matricula e Senha" style={{ marginTop: '25px', marginBottom: 0 }}>
         <Form.Item
-          name="Matrícula"
+          name="matricula"
           rules={[{ required: true }]}
           style={{ display: 'inline-block', width: '25%' }}
         >
           <Input placeholder="Matrícula" />
         </Form.Item>
         <Form.Item
-          name="Senha"
+          name="password"
           rules={[{ required: true }]}
           style={{ display: 'inline-block', width: '25%', margin: '0 8px' }}
         >
           <Input.Password placeholder="Senha" />
         </Form.Item>
       </Form.Item>
-      
-      <div style={{display: 'flex', margin: '0 auto', border: '1px solid #ccc', borderRadius: '20px', width: 'max-content', padding: '15px'}}>
-        {horarios.map((horario, indice: number) => (
-          <div style={{display: 'flex'}} key={horario.weekDay}>
-            <div>
-              <Checkbox onChange={(event) => onChangeSemana(event.target.value)} value={horario.weekDay}>
-                {horario.weekDay}
-              </Checkbox>
-              <Checkbox.Group options={horario.horas} style={{display: 'flex', flexDirection: 'column'}} onChange={onChangeSemana}/>
-            </div>
-            {horarios.length - 1 > indice && (<Divider type='vertical' style={{height: '100%'}}/>)}
-          </div>
-        ))}
-      </div>
-      <br />
+
+      <Form.Item style={{margin: '0 auto'}} >
+        <Select style={{display: 'inline-block', width: '10%', marginRight: '10px'}} placeholder="Selecione um dia" onChange={onChangeSemana}>
+          {renderWeekDaySelect()}
+        </Select>
+
+        <Select style={{display: 'inline-block', width: '10%'}} placeholder="Selecione um dia" onChange={onChangeHours}>
+          {renderHoursSelect()}
+        </Select>
+      </Form.Item>
               
       <Form.Item label=" " colon={false} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <Button type="primary" style={{background: '#00AFEF', borderRadius: '50px'}}>
+        <Button type="primary" htmlType='submit' style={{background: '#00AFEF', borderRadius: '50px'}}>
           Cadastrar
         </Button>
       </Form.Item> 
