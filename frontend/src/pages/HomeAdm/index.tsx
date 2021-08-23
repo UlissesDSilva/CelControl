ab88b6b21982f8bd2feb1e273d7252dd4a10515d
Ulisses Queiroz D. Silva <uqs.dsilva@gmail.com>
2021-08-22 22:23:36 -0300

    Ajustes

--- frontend/src/pages/HomeAdm/index.tsx
+++ frontend/src/pages/HomeAdm/index.tsx
@@ -1,29 +1,12 @@
 import { Container } from "./styles";
 import { Button } from "../../components/Button";
-import { Link, useHistory, useParams } from 'react-router-dom'
-import { useEffect } from "react";
-import { api } from "../../services/api";
-import { useState } from "react";
+import { Link, useHistory} from 'react-router-dom'
 
 import imgAdm from "../../assets/pacce-symbol.svg";
 
-interface CelulaProps {
-  id_celula: number;
-  name: string;
-  facilitator: string;
-  dias_celulas: string;
-  description: string;
-  hours: number;
-  img?: string;
-}
-
 export function HomeAdm() {
-  const params = useParams() as { idCelula: string };
-  const celulaId = params.idCelula;
 
   const history = useHistory();
-
-  const [celula, setCelula] = useState({} as CelulaProps);
 
   function handleNavigateToListCelulas() {
     history.push(`/listCelulas`);
